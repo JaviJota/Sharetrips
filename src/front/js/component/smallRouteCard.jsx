44c83753d61a41144f5dd2ceb9c0dc80578ade85
ivanzr17 <139049435+ivanzr17@users.noreply.github.com>
2024-08-30 17:01:47 +0000

    Css ajustado

--- src/front/js/component/smallRouteCard.jsx
+++ src/front/js/component/smallRouteCard.jsx
@@ -4,7 +4,7 @@ import "../../styles/smallRouteCard.css";
 const SmallRouteCard = ({ img, title}) => {
   return (
     <div className="smallRouteCard d-flex align-items-center p-3 mb-4">
-      <img src={img} alt=""/>
+      <img className="smallroute" src={img} alt=""/>
       <h5 className="m-0 fw-bold">{title}</h5>
     </div>
   );
