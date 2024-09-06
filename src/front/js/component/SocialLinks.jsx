a108b934f220f87759ad4c445e15fe0c474f08af
Luis <166702635+LuisIannelloDev@users.noreply.github.com>
2024-09-06 09:58:46 +0000

    Update Responsive 6-09-2024

--- src/front/js/component/SocialLinks.jsx
+++ src/front/js/component/SocialLinks.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import "../../styles/profileCard.css";
+
 
 const SocialLinks = ({ data }) => {
   const [loading, setLoading] = useState(true); 
@@ -11,17 +13,17 @@ const SocialLinks = ({ data }) => {
         <div>
           {data?.social_media.instagram && (
             <a href={data?.social_media.instagram.startsWith('http') ? data?.social_media.instagram : `https://${data?.social_media.instagram}`} target="_blank" rel="noopener noreferrer">
-              <i className="fa-brands fa-instagram fa-2x"></i>
+              <i className="redes-sociales fa-brands fa-instagram fa-2x"></i>
             </a>
           )}
           {data?.social_media.x && (
             <a href={data?.social_media.x.startsWith('http') ? data?.social_media.x : `https://${data?.social_media.x}`} target="_blank" rel="noopener noreferrer">
-              <i onClick={console.log(data?.social_media.x)} className="fa-brands fa-x-twitter fa-2x"></i>
+              <i onClick={console.log(data?.social_media.x)} className="redes-sociales fa-brands fa-x-twitter fa-2x"></i>
             </a>
           )}
           {data?.social_media.facebook && (
             <a href={data?.social_media.facebook.startsWith('http') ? data?.social_media.facebook : `https://${data?.social_media.facebook}`} target="_blank" rel="noopener noreferrer">
-              <i className="fa-brands fa-facebook fa-2x"></i>
+              <i className="redes-sociales fa-brands fa-facebook fa-2x"></i>
             </a>
           )}
          
--- src/front/js/component/userRoutes.jsx
+++ src/front/js/component/userRoutes.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { USER_ROUTES } from "./data/userRoutes";
 import BlueButton from "./buttons/blueButton.jsx";
 import SmallRouteCard from "./smallRouteCard.jsx";
+import "../../styles/profileCard.css";
 import { Link } from "react-router-dom";
+
 
 const UserRoutes = ({ data, id }) => {
   const user = JSON.parse(localStorage.getItem("user"));
@@ -10,8 +12,8 @@ const UserRoutes = ({ data, id }) => {
 
   return (
     <>
-      <div className="userRoutes ms-3 my-4 justify-content-center ">
-        <h2 className="text-center fw-bold pt-5 pb-3">Mis Rutas</h2>
+      <div className="userRoutes ms-1 my-4 justify-content-center ">
+        <h2 className="text-center fw-bold pt-5 pb-2">Mis Rutas</h2>
         {data?.map((route, index) => (
           <SmallRouteCard
             img={route.images?.img[0]}
@@ -24,10 +26,10 @@ const UserRoutes = ({ data, id }) => {
           ""
         ) : (
           <>
-            <span className="fw-bold fs-5 d-flex justify-content-center">
+            <span className="nueva-ruta fw-bold fs-4 d-flex justify-content-center">
               Crear nueva ruta
             </span>
-            <span className="d-flex justify-content-center mt-3 mb-5">
+            <span className="agregar-ruta d-flex justify-content-center mt- mb-5">
               <Link to={"/route/create"}>
                 <BlueButton buttonName={<i className="fa-solid fa-plus"></i>} />
               </Link>
